1c297543493675d9ff07f0967dbf5353355c8a74
Nomolos29 <86138279+Nomolos29@users.noreply.github.com>
2024-07-06 06:09:18 +0100

    Updated NavItems

--- src/Components/NavItems.tsx
+++ src/Components/NavItems.tsx
@@ -3,15 +3,17 @@ import Link from "next/link"
 import Image from "next/image"
 
 type navProps = {
-href: string;
-src: string;
-name: string;
+    href: string;
+    src: string;
+    name: string;
+    textStyle: string;
+    linkStyle: string;
 }
 
-const NavItems:React.FC<navProps> = ({href, src, name}) => (
-    <Link href={href} className="flex items-center gap-3 p-3 pl-4 rounded-md hover:bg-[#242529]">
+const NavItems:React.FC<navProps> = ({href, src, name, textStyle, linkStyle}) => (
+    <Link href={href} className={`flex items-center gap-3 p-3 pl-4 rounded-md ${linkStyle}`}>
     <Image src={src} alt={name} />
-    <h4 className="text-[18px] text-[#d8d8d8] hover:text-white">{name}</h4>
+    <h4 className={`text-[18px] ${textStyle}`}>{name}</h4>
     </Link>
 )
 
