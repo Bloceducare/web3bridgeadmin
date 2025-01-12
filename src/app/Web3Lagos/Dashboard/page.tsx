7173afb513387446b9e92295f7856e5da0d055f5
Amiola Demilade <amiolademilade@gmail.com>
2025-01-12 22:01:37 +0100

    Add new course

--- src/app/Web3Lagos/Dashboard/page.tsx
+++ src/app/Web3Lagos/Dashboard/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { ScaleLoader } from "react-spinners";
 
@@ -29,6 +30,7 @@ type FormData = {
   venue: ("online" | "onsite")[]; 
   extra_info: string;
   images: Image[];
+  [key: string]: any;
 };
 
 type FormErrors = {
@@ -45,14 +47,19 @@ const initialFormState: FormData = {
 
 const initialFormErrors: FormErrors = {};
 
+
+
 export default function Dashboard() {
   const [formData, setFormData] = useState<FormData>(initialFormState)
       const [errors, setErrors] = useState<FormErrors>(initialFormErrors);
   const [programs, setPrograms] = useState<Program[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [message, setMessage] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [expandedDescriptionId, setExpandedDescriptionId] = useState<number | null>(null); 
   const [isNewCourseOpen, setIsNewCourseOpen] = useState<boolean>(false); 
+
+
   
   useEffect(() => {
     const token = localStorage.getItem("token")
@@ -116,22 +123,104 @@ export default function Dashboard() {
   }
 
 
-  const handleChange = ( e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+  
+    if (e.target instanceof HTMLInputElement && e.target.type === "checkbox") {
+      const checked = e.target.checked;
+  
+      setFormData((prevData) => {
+        const currentValues = Array.isArray(prevData[name])
+          ? prevData[name]
+          : [];
+        if (checked) {
+          return {
+            ...prevData,
+            [name]: [...currentValues, value],
+          };
+        } else {
+          return {
+            ...prevData,
+            [name]: currentValues.filter((item) => item !== value),
+          };
+        }
+      });
+    } else {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: undefined,
+    }));
+  };
+  
+  
+  
+  
+
+  const handleChangePic = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, files } = e.target;
+  
+    if (files && files[0]) {
+      setFormData({
+        ...formData,
+        [name]: [...formData.images, files[0]], 
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: [], 
+      });
+    }
+  
     setErrors({
       ...errors,
       [name]: undefined,
     });
+  };
+
+  const openandCloseCourse = () => {
+    setIsNewCourseOpen((prev) => !prev);
+  }
+  
+
+  const handleNewCourse = async (e: React.FormEvent) => {
+    console.log("clicked")
+    e.preventDefault();
+    setMessage("");
+    setErrors(initialFormErrors); 
+
+    const formDataToSend = {
+      ...formData,
+    };
+
+    console.log(formDataToSend)
+    try {
+      const response = await axios.post("https://web3bridgewebsitebackend.onrender.com/api/v2/cohort/course/", formDataToSend)
+      const data = response.data
+      if (data) {
+     setMessage("Course Created succesfully")
+    console.log("course created")
+    const timer = setTimeout(() => {
+      openandCloseCourse();
+    }, 3000);
+    setFormData(initialFormState);
+      } else {
+        setMessage("Unable to create Course now. We are working on it")
+      }
+    } catch (error) {
+      console.log(error)
+      setMessage("Network error. Please try again later")
+    }
   }
 
-  const handleNewCourse = () => {
-    console.log("clicked")
-    setIsNewCourseOpen((prev) => !prev); 
-  }
+  
 
   return (
     <div className="bg-green-200 w-full h-full p-10">
@@ -139,8 +228,8 @@ export default function Dashboard() {
 
       <div className="space-y-10">
         <div className="flex justify-between">
-        <h1 className="text-center text-3xl font-bold">All Courses</h1>
-        <button className="flex justify-end items-center bg-[#2b292c] p-3 rounded-md text-white" onClick={handleNewCourse}>New Course </button>
+        <h1 className="text-center text-3xl font-bold"> {isNewCourseOpen ? "Create New Course" : "All Courses"}</h1>
+        <button className="flex justify-end items-center bg-[#2b292c] p-3 rounded-md text-white" onClick={openandCloseCourse}>New Course </button>
         </div>
 
           {/* New Course Form */}
@@ -160,7 +249,7 @@ export default function Dashboard() {
               </div>
               <div className="space-y-3">
                 <label>Description</label>
-                <input type="text" name="description" id="" value={formData.description} onChange={handleChange} className="border w-full h-[20vh]" />
+              <p className="border w-full h-[20vh]">  <input type="text" name="description" id="" value={formData.description} onChange={handleChange} className="w-full h-[10vh] outline-none"  /></p>
               </div>
 
               <div className="space-y-3">
@@ -168,28 +257,52 @@ export default function Dashboard() {
                 <input type="text" name="extra_info" id="" value={formData.extra_info} onChange={handleChange} className="border w-full h-[10vh]" />
               </div>
 
+              <div className="space-y-3">
               
-              {/* <div className="space-y-3">
-                <label>Select Image</label>
-                <input type="file" name="image" id="" value={formData.images} onChange={handleChange} className="border w-full h-[10vh]" />
-              </div> */}
+                 <label>Select Image</label>
+              <input
+                type="file" name="images"    id="" onChange={handleChangePic}  className=""/>
+                 </div>
 
-              <button
-                type="submit"
-                className="mt-4 p-3 bg-blue-500 text-white rounded"
-              >
-                Add Course
-              </button>
+
+        <div className="space-y-5">
+          <div className="space-x-2">
+          <input
+              type="checkbox"
+              name="venue"
+              value="online"
+              id="online"
+              onChange={handleChange}
+            />
+            <label htmlFor="online">Online</label>
+          </div>
+
+          <div className="space-x-2">
+          <input
+              type="checkbox"
+              name="venue"
+              value="onsite"
+              id="onsite"
+              onChange={handleChange}
+            />
+            <label htmlFor="onsite">Onsite</label>
+          </div>
+          </div>
+
+          <div className="flex justify-between">
+          <button type="submit"   className="mt-4 p-3 bg-blue-500 text-white rounded"  > Add Course  </button>
+          <button className="mt-2 text-red-500"  onClick={openandCloseCourse} >Cancel </button>
+          </div>
+
+          <div className="flex justify-center text-xl">
+              {message ? <p>{message}</p> : "" }
+              </div>
+
             </form>
-            <button
-              className="mt-2 text-red-500"
-              onClick={() => setIsNewCourseOpen(false)}
-
-            >
-              Cancel
-            </button>
           </div>
         )}
+
+        {/*End of new course form */}
 
 
 
