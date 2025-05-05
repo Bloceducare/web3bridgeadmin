52d621bd40c1a71e3e4b5102398a8a783f328d90
Amiola Demilade <amiolademilade@gmail.com>
2025-05-05 04:23:38 +0100

    feat: add confirmation email functionality and improve participant fetching logic

--- src/hooks/participants.ts
+++ src/hooks/participants.ts
@@ -3,17 +3,17 @@ import { Participant, ApiResponse } from "@/hooks/interface";
 import { useParticipantsStore } from "@/stores/useParticipantsStore";
 
 export const useParticipants = () => {
-  const { addParticipants, setLoading, setError } = useParticipantsStore();
+  const { addParticipants, setParticipants, setLoading, setError } = useParticipantsStore();
 
   const fetchParticipants = useCallback(
     async (token: string) => {
       try {
         setLoading(true);
         setError(null);
-
+        setParticipants([]); 
         let nextUrl: string | null =
           "https://web3bridgewebsitebackend.onrender.com/api/v2/cohort/participant/all/";
-
+  
         while (nextUrl) {
           const response = await fetch(nextUrl, {
             method: "GET",
@@ -23,15 +23,15 @@ export const useParticipants = () => {
             },
             cache: "no-store",
           });
-
+  
           if (!response.ok) {
             throw new Error(`API call failed: ${response.statusText}`);
           }
-
+  
           const result: ApiResponse = await response.json();
-
+  
           if (result.success) {
-            addParticipants(result.data.results);
+            addParticipants(result.data.results); 
             nextUrl = result.data.next;
           } else {
             throw new Error("Failed to fetch participants");
@@ -45,8 +45,39 @@ export const useParticipants = () => {
         setLoading(false);
       }
     },
-    [addParticipants, setLoading, setError]
+    [addParticipants, setParticipants, setLoading, setError]
   );
 
-  return { fetchParticipants };
+  const sendConfirmationEmail = useCallback(
+    async (token: string, email: string) => {
+      console.log ("Sending confirmation email to:", JSON.stringify(email));
+      try {
+        const response = await fetch(
+          "https://web3bridgewebsitebackend.onrender.com/api/v2/cohort/participant/send-confirmation-email/",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `${token}`,
+            },
+            body: JSON.stringify({ email }),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to send confirmation email");
+        }
+
+        const data = await response.json();
+        console.log("Email sent successfully:", data);
+      } catch (error) {
+        console.error("Error sending confirmation email:", error);
+      }
+    },
+    []
+  );
+
+  
+
+  return { fetchParticipants, sendConfirmationEmail };
 };
